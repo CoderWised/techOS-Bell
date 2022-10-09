c02ff99dd97646f35729439d2c012fbaabb366a3
Faeka <faeka6@gmail.com>
2022-10-10 04:01:47 +0530

    Site First Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Automatic Slideshow - change image every 3 seconds
+var myIndex = 0;
+carousel();
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000);
+}
