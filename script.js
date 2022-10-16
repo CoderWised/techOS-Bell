e47ad5092cc9a0c58a682719704b9fb75c6b25cd
Adityageek <adityadixitjbl@gmail.com>
2022-10-16 17:03:14 +0530

    Add Cursor Effects #16

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ function carousel() {
     x[myIndex - 1].style.display = "block";
     setTimeout(carousel, 3000);
 }
+const cursor = document.querySelector(".cursor");
+var timeout;
+
+//follow cursor on mousemove
+document.addEventListener("mousemove", (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+
+  cursor.style.top = y + "px";
+  cursor.style.left = x + "px";
+  cursor.style.display = "block";
+
+  //cursor effects when mouse stopped
+  function mouseStopped(){
+    cursor.style.display = "none";
+  }
+  clearTimeout(timeout);
+  timeout = setTimeout(mouseStopped, 1000);
+});
+
+    //cursor effects when mouseout
+    document.addEventListener("mouseout", () => {
+        cursor.style.display = "none";
+      });
