298171ba9a54075d6e360c3a65e35f080ee09ec0
Ben Sivyer <bensivyer@Bens-MacBook-Pro.local>
2022-10-20 12:27:30 +0100

    add faq section

--- script.js
+++ script.js
@@ -36,3 +36,19 @@ document.addEventListener("mousemove", (e) => {
     document.addEventListener("mouseout", () => {
         cursor.style.display = "none";
       });
+
+//FAQ accordian effects
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
